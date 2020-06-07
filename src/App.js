cb1a006d397bdf1430602a6d464b114563f05e4d
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-06-08 03:09:52 +0530

    Initialized MainPage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div>
-      
+		<MainPage />      
     </div>
   );
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+	constructor(props){
+		super(props); // recommended.
+
+	}
+	render() {
+		return(
+			<div className="container"> 
+				
+			</div>
+		);
+	}
+}
+
+export default MainPage;
